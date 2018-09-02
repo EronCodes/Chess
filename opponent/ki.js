5eb1cc0af0d58d0a813c05e2205b710fb299d481
Ivo Z <ivo.ziesche@web.de>
2018-09-02 20:46:49 +0200

    Now you dont have side, now you have inverted The board will inverted at the beginnig and the ending if you are the minus side

--- opponent/ki.js
+++ opponent/ki.js
@@ -4,15 +4,45 @@ function Ki(side) {
   -1 - -6 Black player
   */
 
-  this.side = side;
+  /* Chess Board
+  [[2, 3, 4, 5, 6, 4, 3, 2],
+   [1, 1, 1, 1, 1, 1, 1, 1],
+   [0, 0, 0, 0, 0, 0, 0, 0],
+   [0, 0, 0, 0, 0, 0, 0, 0],
+   [0, 0, 0, 0, 0, 0, 0, 0],
+   [0, 0, 0, 0, 0, 0, 0, 0],
+   [-1, -1, -1, -1, -1, -1, -1, -1],
+   [-2, -3, -4, -5, -6, -4, -3, -2]]
+  */
+
+  this.invertBoard = function (board) {
+    for (var i = 0; i < board.length; i++) {
+      for (var k = 0; k < board[i].length; k++) {
+        board[i][k] = board[i][k] * (-1);
+      }
+    }
+    return board;
+  }
+
+  if (side == 1) {
+    this.inverted = false;
+  } else {
+    this.inverted = true;
+  }
 
   this.play = function (board) {
-    var ownPlayers    = this.getOwnPlayers(board, side);
-    var player        = this.ownPlayers[Math.floor(Math.random() * ownPlayers.length)];
+    if (this.inverted) {
+      var board         = this.invertBoard(board);
+    }
+    var ownPlayers    = this.getOwnPlayers(board);
+    var player        = ownPlayers[Math.floor(Math.random() * ownPlayers.length)];
     var movingOptions = this.getMovingOptions(player, board);
     var step          = movingOptions[Math.floor(Math.random() * ownPlayers.length)];
     var resultBoard   = this.makeStep(player, step, board);
 
+    if (this.inverted) {
+      board = this.invertBoard(board)
+    }
     return resultBoard;
   };
 
@@ -20,21 +50,12 @@ function Ki(side) {
     var ownPlayers = [];
 
     for (var i = 0; i < board.length; i++) {
-      for (var k = 0; k < board[0].length; k++) {
-        if (this.side == 1) {
-          if (board[i][k] > 0) {
-            ownPlayers[ownPlayers.length] = {posX: i,
-                                             posY: k,
-                                             kind: board[i][k]
-                                            };
-          }
-        } else {
-          if (board[i][k] < 0) {
-            ownPlayers[ownPlayers.length] = {posX: i,
-                                             posY: k,
-                                             kind: board[i][k]
-                                            };
-          }
+      for (var k = 0; k < board[i].length; k++) {
+        if (board[i][k] > 0) {
+          ownPlayers[ownPlayers.length] = {posX: i,
+                                           posY: k,
+                                           kind: board[i][k]
+                                          };
         }
       }
     }
