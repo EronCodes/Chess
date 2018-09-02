f3bec8db9264fa9c7d1c72a0c334acc8381c4757
Ivo Z <ivo.ziesche@web.de>
2018-09-02 19:16:11 +0200

    Add skeletal structure of ki.js

---
+++ opponent/ki.js
@@ -0,0 +1,13 @@
+function Ki() {
+  this.getOpponents = function (board, opponent) {
+
+  };
+
+  this.getMovingOptions = function (player, board) {
+
+  };
+
+  this.makeStep = function (player, step, board) {
+
+  };
+}
