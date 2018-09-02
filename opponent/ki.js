1666955abc171f36d953e77e1cee3110e0c6510c
Ivo Z <ivo.ziesche@web.de>
2018-09-02 19:37:51 +0200

    Add getOwnPlayers() functions

--- opponent/ki.js
+++ opponent/ki.js
@@ -17,7 +17,29 @@ function Ki(side) {
   };
 
   this.getOwnPlayers = function (board) {
+    var ownPlayers = [];
 
+    for (var i = 0; i < board.length; i++) {
+      for (var k = 0; k < board[0].length; k++) {
+        if (this.side == 1) {
+          if (board[i][k] > 0) {
+            ownPlayers[ownPlayers.length] = {posX: i,
+                                             posY: k,
+                                             kind: board[i][k]
+                                            };
+          }
+        } else {
+          if (board[i][k] < 0) {
+            ownPlayers[ownPlayers.length] = {posX: i,
+                                             posY: k,
+                                             kind: board[i][k]
+                                            };
+          }
+        }
+      }
+    }
+
+    return ownPlayers;
   };
 
   this.getMovingOptions = function (player, board) {
