6b471fa12a6a6f5e3c2464e5c3c3187d7efd9a10
Ivo04 <ivo.ziesche@web.de>
2018-09-06 20:22:56 +0200

    Add posValid function

--- opponent/movingOptions.js
+++ opponent/movingOptions.js
@@ -22,4 +22,11 @@ function MovingOptions() {
   this.king = function (x, y, board) {
 
   };
+
+  this.posValid = function (x, y, board) {
+    if (x < 0 || x > 7 || y < 0 || y > 7) {
+      return false;
+    }
+    return true;
+  };
 }
