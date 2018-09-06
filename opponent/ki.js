8929818efb475d1353584e9acfa853a7ccb5daef
Ivo04 <ivo.ziesche@web.de>
2018-09-06 20:17:24 +0200

    Fix bugs and logic errors

--- opponent/ki.js
+++ opponent/ki.js
@@ -5,26 +5,29 @@ function Ki(side) {
   */
 
   /* Chess Board
-  [[2, 3, 4, 5, 6, 4, 3, 2],
-   [1, 1, 1, 1, 1, 1, 1, 1],
-   [0, 0, 0, 0, 0, 0, 0, 0],
-   [0, 0, 0, 0, 0, 0, 0, 0],
-   [0, 0, 0, 0, 0, 0, 0, 0],
-   [0, 0, 0, 0, 0, 0, 0, 0],
+  [[-2, -3, -4, -5, -6, -4, -3, -2],
    [-1, -1, -1, -1, -1, -1, -1, -1],
-   [-2, -3, -4, -5, -6, -4, -3, -2]]
+   [0, 0, 0, 0, 0, 0, 0, 0],
+   [0, 0, 0, 0, 0, 0, 0, 0],
+   [0, 0, 0, 0, 0, 0, 0, 0],
+   [0, 0, 0, 0, 0, 0, 0, 0],
+   [1, 1, 1, 1, 1, 1, 1, 1],
+   [2, 3, 4, 5, 6, 4, 3, 2]]
   */
 
   this.invertBoard = function (board) {
+    console.log("invert called");
     for (var i = 0; i < board.length; i++) {
       for (var k = 0; k < board[i].length; k++) {
         board[i][k] = board[i][k] * (-1);
       }
     }
+    board = this.rotateBoard(board);
     return board;
   }
 
   this.rotateBoard = function (board) {
+    console.log("rotate called");
     board.reverse();
     for (var i = 0; i < board.length; i++) {
       board[i].reverse();
@@ -32,7 +35,7 @@ function Ki(side) {
     return board;
   }
 
-  if (side == 1) {
+  if (side > 0/*side == white*/) {
     this.inverted = false;
   } else {
     this.inverted = true;
@@ -42,7 +45,7 @@ function Ki(side) {
 
   this.play = function (board) {
     if (this.inverted) {
-      var board         = this.invertBoard(board);
+      var board       = this.invertBoard(board);
     }
     var ownPlayers    = this.getOwnPlayers(board);
     var player        = ownPlayers[Math.floor(Math.random() * ownPlayers.length)];
